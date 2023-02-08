ae2d807c7c4701907d2b35b5173ffdb79176bb15
i_mml <mohamadja272@gmail.com>
2023-02-08 15:36:00 +0330

    declare  the _id field in user struct

--- model/user/user.go
+++ model/user/user.go
@@ -3,7 +3,7 @@ package user
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	Id          primitive.ObjectID
+	Id          primitive.ObjectID `bson:"_id"`
 	FirstName   string
 	LastName    string
 	Age         int
