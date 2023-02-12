50c8a9588228e261b2fc0dd686737b240248d207
i_mml <mohamadja272@gmail.com>
2023-02-12 15:00:58 +0330

    edit user struct

--- model/user/user.go
+++ model/user/user.go
@@ -1,11 +1,15 @@
 package user
 
+import "time"
+
 type User struct {
-	Id          string `bson:"_id,omitempty"`
-	FirstName   string `bson:"firstName,omitempty"`
-	LastName    string `bson:"lastName,omitempty"`
-	Age         int    `bson:"age,omitempty"`
-	PhoneNumber string `bson:"phoneNumber,omitempty"`
+	Id           string    `bson:"_id,omitempty"`
+	FirstName    string    `bson:"firstName,omitempty"`
+	LastName     string    `bson:"lastName,omitempty"`
+	Email        string    `bson:"email,omitempty"`
+	UserName     string    `bson:"userName,omitempty"`
+	Password     string    `bson:"password,omitempty"`
+	RegisterData time.Time `bson:"registerDate,omitempty"`
 
 	//PhoneNumber string `query:"phone" json:"mobile"`
 }
